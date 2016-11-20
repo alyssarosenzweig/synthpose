81b2319cfaaa10cddfdeedafa0d02ad7e5764316
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-20 15:41:08 -0800

    Test render

--- synth.py
+++ synth.py
@@ -1,2 +1,14 @@
-import bpy
-print(bpy.data.objects["Cube"])
+import bpy, random
+
+random.seed(None) # seed RNG with time
+
+for obj in bpy.data.objects:
+    if obj.type == 'CAMERA':
+        print(obj.rotation_euler)
+        # obj.rotation_euler = [0, 0, 0]
+#bpy.context.camera.shift_x = random.uniform(-10, 10)
+#bpy.context.scene.render()
+
+bpy.ops.render.render(write_still=True)
+
+print("Scene ready for render!")
