d2635a2942aa3328a3879fd177aacb9b25ea6160
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-20 22:29:53 -0800

    Code cleanup

--- synth.py
+++ synth.py
@@ -1,7 +1,5 @@
 import bpy, random
 
-random.seed(None) # seed RNG with time
-
 rad2deg = 360 / 6.28
 
 def euler_rotate(bone):
@@ -11,14 +9,9 @@ def euler_rotate(bone):
             random.gauss(0, 0.5),
             random.gauss(0, 0.5)]
 
-def set_mode(mode):
-    if bpy.ops.object.mode_set.poll():
-        bpy.ops.object.mode_set(mode=mode)
-
-# randomize camera location a bit
 for obj in bpy.data.objects:
     if obj.type == 'CAMERA':
-        # TODO: rotate camera according
+        # TODO: rotate camera randomly, according to paper
         pass
     
     # TODO: generalize to other models
@@ -26,23 +19,11 @@ for obj in bpy.data.objects:
         obj.rotation_mode = 'XYZ'
         obj.rotation_euler = [-30 / rad2deg, 0 / rad2deg, 45 / rad2deg]
 
-#        set_mode("POSE")
-
         for bone in obj.pose.bones:
             print(bone.name)
 
             if bone.name.split(".")[0] in ["Upperarm", "Lowerarm", "Thigh", "Shin", "Hand", "Fingers1", "Fingers2"]:
                 euler_rotate(bone)
-
-#        mat = bpy.data.materials.new("RED")
-#        mat.diffuse_intensity = 1.0
-#        mat.diffuse_shader = 'LAMBERT'
-#        mat.diffuse_color = [1.0, 0.0, 0.0]
-#        obj.active_material = mat
-
-# fix some rendering issues
-
-bpy.data.worlds[0].light_settings.use_indirect_light = True
 
 bpy.ops.render.render(write_still=True)
 
