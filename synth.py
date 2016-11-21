8154c3cc4e85084d3440193e667279965cbdedc0
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-21 11:03:08 -0800

    Cleanup code as per Blender tooltips

--- synth.py
+++ synth.py
@@ -7,32 +7,32 @@ def euler_rotate(bone):
             random.gauss(0, 0.25),
             random.gauss(0, 0.1)]
 
-for obj in bpy.data.objects:
-    if obj.type == 'CAMERA':
-        # TODO: rotate camera randomly, according to paper
-        obj.rotation_mode = 'XYZ'
-        obj.rotation_euler = [3.14/3, 0, 0]
-        obj.location = [0.0, -4.0, 3.0]
-        pass
+# TODO: rotate camera randomly, according to paper
+camera = bpy.data.objects["Camera"]
+camera.rotation_mode = 'XYZ'
+camera.rotation_euler = [3.14/3, 0, 0]
+camera.location = [0.0, -4.0, 3.0]
     
-    # TODO: generalize to other models
-    if obj.name == 'MaleArm':
-        # random rotation of person
-        obj.rotation_mode = 'XYZ'
-        obj.rotation_euler = [0, 0, random.gauss(0, 3.14/8)]
+# TODO: generalize to other models
+model = bpy.data.objects["MaleArm"]
 
-        obj.scale[0] = random.gauss(1, 0.15) # random weight
-        obj.scale[1] = random.gauss(1, 0.15) # random, uh, depth
-        obj.scale[2] = random.gauss(1, 0.15) # random height
+# random rotation of person
+model.rotation_mode = 'XYZ'
+model.rotation_euler = [0, 0, random.gauss(0, 3.14/8)]
 
-        bpy.data.materials["MaleSkin"].use_textures[0] = True
-        bpy.data.materials["MaleSkin"].use_textures[1] = False
+model.scale[0] = random.gauss(1, 0.15) # random weight
+model.scale[1] = random.gauss(1, 0.15) # random, uh, depth
+model.scale[2] = random.gauss(1, 0.15) # random height
 
-        for bone in obj.pose.bones:
-            print(bone.name)
+mat = bpy.data.materials["MaleSkin"]
+mat.use_textures[0] = True
+mat.use_textures[1] = False
 
-            if bone.name.split(".")[0] in ["Upperarm", "Lowerarm", "Thigh", "Shin", "Hand", "Fingers1", "Fingers2"]:
-                euler_rotate(bone)
+for bone in model.pose.bones:
+    print(bone.name)
+
+    if bone.name.split(".")[0] in ["Upperarm", "Lowerarm", "Thigh", "Shin", "Hand", "Fingers1", "Fingers2"]:
+        euler_rotate(bone)
 
 # depth implemented by hacking mist
 bpy.data.worlds[0].mist_settings.use_mist = False
