ecc9826bbc9d50a2a3471750b0847ff7f8e14b5a
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-20 18:56:30 -0800

    Generate random poses

--- synth.py
+++ synth.py
@@ -2,8 +2,37 @@ import bpy, random
 
 random.seed(None) # seed RNG with time
 
+rad2deg = 360 / 6.28
+
+def euler_rotate(bone):
+    bone.rotation_mode = 'XYZ'
+    bone.rotation_euler = [
+            random.uniform(-2.5, +2.5),
+            random.uniform(-2.5, +2.5),
+            random.uniform(-2.5, +2.5)]
+
+# randomize camera location a bit
 for obj in bpy.data.objects:
     if obj.type == 'CAMERA':
-        obj.rotation_euler[1] += 0.5
+        #obj.rotation_euler[0] += random.uniform(-1, 1)
+        #obj.rotation_euler[1] += random.uniform(-1, 1)
+        #obj.rotation_euler[2] += random.uniform(-1, 1)
+        pass
+    if obj.name == 'MaleArm':
+        obj.rotation_mode = 'XYZ'
+        obj.rotation_euler = [-30 / rad2deg, 0 / rad2deg, 45 / rad2deg]
+
+        if bpy.ops.object.mode_set.poll():
+            bpy.ops.object.mode_set(mode='POSE')
+
+        for bone in obj.pose.bones:
+            print(bone.name)
+
+            if bone.name.split(".")[0] in ["Upperarm", "Lowerarm", "Thigh", "Shin", "Hand", "Fingers1", "Fingers2"]:
+                euler_rotate(bone)
+
+        print(obj)
+
+bpy.ops.render.render(write_still=True)
 
 print("Scene ready for render!")
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-blender --background --python synth.py -o //out.png -f 1 cube.blend
-iceweasel out.png*
+blender human_1.blend --python synth.py -b
+iceweasel /tmp/.png
